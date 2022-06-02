74f2176a46a07bb65b577041d33cc514e763b25e
hjy-kor <jyhwang118@gmail.com>
2022-06-02 12:49:03 +0900

    0602

--- back/server/recommend/urls.py
+++ back/server/recommend/urls.py
@@ -4,4 +4,5 @@ from rest_framework import urls
 
 urlpatterns = [
     path('select/<str:pk>', views.ingre_list),
+    path('recommend', views.ingre_combi)
 ]
--- back/server/recommend/views.py
+++ back/server/recommend/views.py
@@ -25,27 +25,34 @@ def ingre_list(request, pk):
         # return JsonResponse(serializer.data, safe=False)
 
 
-def ingre_combi(request, pk):
+@csrf_exempt
+def ingre_combi(request):
     # 필요한 데이터
-    df_best_comb_2 = pd.read_csv('재료별최적의궁합2_new.csv')
-    df_lsts = pd.read_csv('재료리스트정리.csv')
-    df_veges = pd.read_csv('채식주의자종류.csv')
+    df_best_comb_2 = pd.read_excel(
+        '/Users/hwangjeong-yeon/Desktop/코딩/파이썬/종설_프로젝트/Capstone-food-recommender-system_hjy/back/server/recommend/재료별최적의궁합_glv_top5.xlsx')
+    df_lsts = pd.read_csv(
+        '/Users/hwangjeong-yeon/Desktop/코딩/파이썬/종설_프로젝트/Capstone-food-recommender-system_hjy/back/server/recommend/재료리스트정리.csv')
+    df_veges = pd.read_csv(
+        '/Users/hwangjeong-yeon/Desktop/코딩/파이썬/종설_프로젝트/Capstone-food-recommender-system_hjy/back/server/recommend/채식주의자종류.csv')
 
     # 유저 아이디로 정보 가져오기
     # 리스트가 텍스트로 오므로 이것을 다시 리스트화 하기
-    obj = User.objects.get(id=pk)
-    vege_kinds_raw = obj.vegtype
-    inedible_groups_raw = obj.allergic
-    jsonDec = json.decoder.JSONDecoder()
-    vege_kinds = jsonDec.decode(vege_kinds_raw)
-    inedible_groups = jsonDec.decode(inedible_groups_raw)
+    # obj = User.objects.get(id=pk)
+    # vege_kinds_raw = obj.vegtype
+    # inedible_groups_raw = obj.allergic
+    # jsonDec = json.decoder.JSONDecoder()
+    # vege_kinds = jsonDec.decode(vege_kinds_raw)
+    # inedible_groups = jsonDec.decode(inedible_groups_raw)
 
     #
     if request.method == 'POST':
         # 재료들 리스트가 이리로 넘어옴{ingres:[1,2,3,4,...]}
-        data = JSONParser().parse(request)
+        #data = JSONParser().parse(request)
         # 재료 리스트를 꺼내주기
-        main = data[0]
+        #main = data[0]
+        main = ["두부"]
+        vege_kinds = []
+        inedible_groups = []
     # 알레르기환자: 못먹는 재료가 바로 리스트(inedible_groups)로 들어옴(알레르기 없으면 빈리스트)
     # 채식주의자: 채식주의자의 종류가 리스트로 들어옴(vege_kinds) -> 종류를 받아서 채식주의자별 못먹는 재료 리스트(vege) 생성
     # 채식주의자 아니면 vege_kinds가 빈리스트 -> vege 생성하지 않음
@@ -59,6 +66,7 @@ def ingre_combi(request, pk):
         # 못먹는 재료 리스트(inedible_groups)와 대체식품 재료 리스트(alters)를 비교
         # 대체도 안되고 최종적으로 제외해야 하는 재료 리스트(indedible) = 대체식품 리스트에 들어있지 않은(대체 안되는) 못먹는 재료 리스트
         #inedible = set(inedible_groups) - set(alters)
+        inedible = set(inedible_groups)
 
         result = []
         for c in df_best_comb_2['best_combination']:
@@ -71,7 +79,7 @@ def ingre_combi(request, pk):
                         '[', '').replace(']', '').replace("'", "").split(',')
                     # 최종적으로 제외해야 하는 재료를 제외하고 최적의 궁합에 있는 모든 재료는 포함하는 레시피 번호
                     # 결과 레시피 번호(변수명: result, 내용: 레시피 번호, 형식:리스트)는 다음 인자로 넘겨줌
-                    if (len(inedible_groups & set(lst_s)) == 0) & set(combi).issubset(set(lst_s)):
+                    if (len(inedible & set(lst_s)) == 0) & set(combi).issubset(set(lst_s)):
                         result.append(df_lsts.loc[i, 'RECIPE_ID'])
         sample_combi_result = result
         recommendations = Recipe.objects.filter(
